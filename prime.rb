112b3d4313dd011d6b2c9802d004cb94dff28846
Bruce Ruffin <bruffin1221@gmail.com>
2019-08-16 19:10:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,31 +1,13 @@
 def prime?(num)
-multiples=[]
-i=2
-while i<num
-  multiples<< ("#{num}".to_i%"#{i}".to_i)
-    i+=1
-  end
-  if multiples.none?(0)
-    true
-  elsif num==2
-    true
-  elsif multiple.include?([])
-    false
-  else
-    false
-  end
-end
-
-def prime?(num)
   multiples=[]
   i=2
   while i<num
     multiples<< ("#{num}".to_i%"#{i}".to_i).zero?
     i+=1
   end
-  if multiples.all?(false) || num==2
+  if num>=2 && multiples.all?(false)
     true
-  elsif multiples==[] || multiples.any?(true)
+  else
     false
   end
 end
